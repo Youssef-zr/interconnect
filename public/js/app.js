5e493535e3eef8bc3086f1c34c105ce4ed135926
youssefzr <yn-neinaa@hotmail.com>
2021-04-14 12:24:37 +0000

    fix email adress

--- public/js/app.js
+++ public/js/app.js
@@ -52454,8 +52454,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\youpel_project_one\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\youpel_project_one\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\interconnect\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\interconnect\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
